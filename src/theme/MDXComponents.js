89aa8f83efab50c0a29ea7398f9ac0e91891ca41
weirdyang <wy4786@gmail.com>
2023-04-30 09:17:50 +0800

    'init'

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,10 @@
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Image from '@theme/IdealImage';
+import Highlight from '../components/HomepageFeatures/Highlight';
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  Highlight,
+  Image
+};
